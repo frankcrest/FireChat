df51791a07f52915a2c38260567496e13d644297
frankcrest <frankcrestview@hotmail.com>
2020-08-06 14:09:19 -0400

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Frank Chen on 2020-08-06.
+//  Copyright © 2020 Westguards Digital Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
